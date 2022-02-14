ccdda6c3d20c70a6a865ed70ab96a580a7fd2027
Alexjandro Mendoza <alexjandromendoza@macbook-pro.lan>
2022-02-13 23:54:42 -0500

    react routing works fine with no issues w v6, forgot to commit this last night

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-
+import React from 'react';
+import Skills from './components/Skills'
+import About from './components/About';
+import Work from './components/Work'
+import Contact from './components/Contact'
+import { BrowserRouter as Router, Link, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <>
-    </>
+    <Router>
+      
+        <Link to="/about"> About </Link>
+        <Link to="/contact"> Contact </Link>
+        <Link to="/skills"> Skills </Link>
+        <Link to="/work"> Work </Link>
+      
+      <Routes>
+        <Route path="/about" element={<About />} />
+        <Route path="/skills" element={<Skills />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/work" element={<Work />} />
+      </Routes>
+      <div>
+        
+      </div>
+    </Router>   
     
   );
 }
---
+++ src/components/About.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+function About() {
+
+  let navigate = useNavigate();
+
+
+
+
+  return <div>
+      About Page 
+  </div>;
+}
+
+export default About;
